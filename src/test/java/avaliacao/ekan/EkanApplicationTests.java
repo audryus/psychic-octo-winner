551e442b5745cc0e0b4485c137289f92aeffcccb
audryus <audryus@gmail.com>
2024-10-02 15:43:23 -0300

    initial commit

---
+++ src/test/java/avaliacao/ekan/EkanApplicationTests.java
@@ -0,0 +1,13 @@
+package avaliacao.ekan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
